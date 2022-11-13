287d813bbd6bc0d635d9b00da001346ce6536e5d
Benjamin Leanna <94625006+Askrr@users.noreply.github.com>
2022-11-12 22:14:48 -0600

    Add files via upload

--- clockMain.cpp
+++ clockMain.cpp
@@ -15,27 +15,42 @@
 using namespace std;
 // Includes the clock displays header file to define everything main() needs
 #include "clockDisplays.h"  
-
-int main() {															       //////////////////////////////////////////////////////////////////
-	int choice = 0;														       /// In the main function, kbhit will return a non-zero integer ///
-	int flag = 0;															   /// if a key is in the keyboard buffer. It will not wait for a ///
-	while (choice != 4) {												   	   /// key to be pressed. This allows the program to continue to  ///
-		while (!kbhit() && flag == 0) {										   /// refresh itself while the user makes a choice for input.    ///
-			system("cls");													   /// system("cls") clears the screen then calls the display()   ///
-			display();														   /// again to update to the new time while adding a second in   ///
-			second++;                                                          /// between each refresh. sleep_for will delay the adding of   ///                                           
-			std::this_thread::sleep_for(std::chrono::milliseconds(1000));      /// the second to not let the clock speed up past real time    ///
-			main();                                                            //////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+/// In the main function, kbhit will return a non-zero integer ///
+/// if a key is in the keyboard buffer. It will not wait for a ///
+/// key to be pressed. This allows the program to continue to  ///
+/// refresh itself while the user makes a choice for input.    ///
+/// system("cls") clears the screen then calls the display()   ///
+/// again to update to the new time while adding a second in   ///
+/// between each refresh. sleep_for will delay the adding of   ///
+/// the second to not let the clock speed up past real time    ///
+//////////////////////////////////////////////////////////////////
+int main() {                                                                   
+	int choice = 0;                                                            
+	int flag = 0;                                                              
+	while (choice != 4) {                                                      
+		while (!kbhit() && flag == 0) {                                        
+			system("cls");                                                     
+			display();                                                         
+			second++;                                                                                                     
+			std::this_thread::sleep_for(std::chrono::milliseconds(1000));      
+			main();                                                            
 		}
 		cin >> choice;
-
-		switch (choice) {                //////////////////////////////////////////////////////
-		case 1:                          /// Standard switch statement for each menu option ///
-			hour = hour + 1;             /// For each case since the sleep timer is 1000    ///
-			second = second - 1;         /// and the timer never stops, so you have to sub  ///
-		case 2:                          /// one second for each choice so the time doesn't ///
-			minute = minute + 1;         /// get out of alignment.                          ///
-			second = second - 1;         //////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+/// Standard switch statement for each menu option ///
+/// For each case since the sleep timer is 1000    ///
+/// and the timer never stops, so you have to sub  ///
+/// one second for each choice so the time doesn't ///
+/// get out of alignment.                          ///
+//////////////////////////////////////////////////////
+		switch (choice) {                
+		case 1:                          
+			hour = hour + 1;             
+			second = second - 1;         
+		case 2:                          
+			minute = minute + 1;         
+			second = second - 1;         
 		case 3:
 			second = second + 1;
 		}
